4a44189ef25b02f8f5a84e740296fd508139db51
Shawn Huelle <shuelle@posteo.de>
2023-09-01 16:44:04 +0200

    fetch api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function HomePage() {
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+const fetcher = (URL) => fetch(URL).then((res) => res.json());
+
+export default function Index() {
+  const { data } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+  console.log(data);
+
+  if (!data) {
+    return "loading...";
+  }
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>My Framework from file</h1>
+      <ul>
+        {data.map((artwork) => (
+          <>
+            <li key={artwork.slug}>{artwork.name}</li>
+            <li>{artwork.artist} </li>
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
